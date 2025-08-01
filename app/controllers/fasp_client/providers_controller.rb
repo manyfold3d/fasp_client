3500d777ec081342bee8e8bd5ce947cab0f1e7b2
James Smith <james@floppy.org.uk>
2025-08-01 16:04:03 +0100

    add registration parameters

--- app/controllers/fasp_client/providers_controller.rb
+++ app/controllers/fasp_client/providers_controller.rb
@@ -1,6 +1,7 @@
 module FaspClient
   class ProvidersController < ApplicationController
     def create
+      params.expect(:name, :baseUrl, :serverId, :publicKey)
       head :created
     end
   end
--- spec/requests/providers_spec.rb
+++ spec/requests/providers_spec.rb
@@ -4,15 +4,28 @@ require "rails_helper"
 
 RSpec.describe "Providers", type: :request do
   describe "POST /registration" do
-    let(:params) { {} }
+    let(:headers) { { "CONTENT_TYPE" => "application/json" } }
+    let(:params) do
+      {
+        "name" => "Example FASP",
+        "baseUrl" => "https://fasp.example.com",
+        "serverId" => "b2ks6vm8p23w",
+        "publicKey" => "FbUJDVCftINc9FlgRu2jLagCVvOa7I2Myw8aidvkong="
+      }
+    end
     it "succeeds" do
-      post "/fasp/registration", params: params
+      post "/fasp/registration", params: params.to_json, headers: headers
       expect(response).to have_http_status :created
     end
 
     it "creates a new provider record"
 
-    [].each do |key|
+    [
+      "name",
+      "baseUrl",
+      "serverId",
+      "publicKey"
+    ].each do |key|
       it "returns bad request if missing #{key} parameter" do
         post "/fasp/registration", params: params.except(key)
         expect(response).to have_http_status :bad_request
